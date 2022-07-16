9d1f36cb50de128ff9a043e9a6223af986b7efc3
Adam Melhem <Adam Melhem@DESKTOP-NVK9QMH>
2022-07-16 18:17:11 +0300

    adding comments note

--- PersonCollection/PersonCollection/Program.cs
+++ PersonCollection/PersonCollection/Program.cs
@@ -1,3 +1,8 @@
-﻿using PersonCollection;
+﻿//SortedList insertion operation and removal operation take O(n).
+//Removes the person object with the maximum value and returns it performed in WC time complexity of O(1).
+//SortedList sorted by max value.
+//Max value can be determined by diffrent stratagies based on some IPerson properties.
+
+using PersonCollection;
 var maximumValueSortStrategy = new MaximumValueSortStrategyByPersonId();
 var personCollection = new PersonSortedCollection(maximumValueSortStrategy);
